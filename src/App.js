fed2444a4e7fd2a3fa3fa31af9173a7618412256
Olivier Tafforin <olivier.tafforin@gmail.com>
2024-11-29 17:04:39 +0100

    api.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React, { useEffect, useState } from 'react';
+import { getHelloMessage } from './services/api';
 function App() {
+
+  const [message, setMessage] = useState('');
+
+
+  useEffect(() => {
+    getHelloMessage().then(setMessage);
+}, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>test v4 : {message}</h1>
     </div>
   );
 }
